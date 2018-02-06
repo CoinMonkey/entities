e60f993f93fadf045aa3622d6c43706fe79c2c7b
pistol88 <pistol88@gmail.com>
2018-02-06 17:05:15 +0300

    added status entity

---
+++ src/Status.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace coinmonkey\entities;
+
+use coinmonkey\interfaces\StatusInterface;
+
+class Status implements StatusInterface
+{
+    private $status;
+    private $tx1Id;
+    private $tx2Id;
+
+    public function __construct(int $status, $tx1Id = null, $tx2Id = null)
+    {
+        $this->status = $status;
+        $this->tx1Id = $tx1Id;
+        $this->tx2Id = $tx2Id;
+    }
+
+    public function getStatus() : int
+    {
+        return $this->status;
+    }
+
+    public function getTxs() : array
+    {
+        return [
+            1 => $this->tx1Id,
+            2 => $this->tx2Id
+        ]
+    }
+}
