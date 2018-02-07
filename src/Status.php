e7d0fc657e0533a217c57813347fad2c4fea8075
pistol88 <pistol88@gmail.com>
2018-02-07 19:24:48 +0300

    refactoring

--- src/Status.php
+++ src/Status.php
@@ -29,4 +29,18 @@ class Status implements StatusInterface
             2 => $this->tx2Id
         ];
     }
+
+    public function getStatusName()
+    {
+        switch($this->status) {
+            case Order::STATUS_FAIL: return 'fail';
+            case Order::STATUS_WAIT_CLIENT_TRANSACTION: return 'wailt client transaction';
+            case Order::STATUS_WAIT_EXCHANGER_PROCESSING: return 'wait exchanger processing';
+            case Order::STATUS_EXCHANGER_PROCESSING: return 'exchanger processing';
+            case Order::STATUS_WAIT_EXCHANGER_TRANSACTION: return 'wait exchanger transaction';
+            case Order::STATUS_DONE: return 'done';
+        }
+
+        return null;
+    }
 }
